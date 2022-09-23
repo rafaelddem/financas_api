0829a151f895c70265a3c8f2e56acd80f3b1149e
Rafael <rafaelddem@gmail.com>
2022-09-23 14:03:41 -0300

    Possibilidade de relatorio, nao foi possivel integrar em um so

--- src/model/businessObject/Reports.php
+++ src/model/businessObject/Reports.php
@@ -3,6 +3,7 @@
 namespace financas_api\model\businessObject;
 
 use DateTime;
+use financas_api\conf\Parameters;
 use financas_api\controller\Response;
 use financas_api\model\dataAccess\Reports as Reports_dataAccess;
 
@@ -11,21 +12,35 @@ class Reports
     private $owner_id;
     private $start_date;
     private $end_date;
+    private $group;
+    private $start_at;
 
     public function __construct(array $parameters = null)
     {
         $this->owner_id = isset($parameters['owner_id']) ? $parameters['owner_id'] : null;
         $this->start_date = isset($parameters['start_date']) ? $parameters['start_date'] : null;
         $this->end_date = isset($parameters['end_date']) ? $parameters['end_date'] : null;
+        $this->group = isset($parameters['group']) ? $parameters['group'] : null;
+        $this->start_at = isset($parameters['start_at']) ? $parameters['start_at'] : null;
     }
 
     public function calculatesTotals()
     {
         try {
+            $this->start_at = (isset($this->start_at) && $this->start_at > 0) ? $this->start_at : Parameters::$FIRST_DAY_OF_MONTH;
+
             $dao = new Reports_dataAccess();
             // $totals = $dao->calculatesWalletsTotals_byPeriod(new DateTime($this->start_date), new DateTime($this->end_date), $this->owner_id);
             // $totals = $dao->calculatesWalletsTotals_byMonths(new DateTime($this->start_date), new DateTime($this->end_date), $this->owner_id);
-            $totals = $dao->calculatesWalletsTotals_byDays(new DateTime($this->start_date), new DateTime($this->end_date), $this->owner_id);
+            // $totals = $dao->calculatesWalletsTotals_byDays(new DateTime($this->start_date), new DateTime($this->end_date), $this->owner_id);
+
+            $totals = $dao->calculatesTotals_sql(
+                new DateTime($this->start_date), 
+                new DateTime($this->end_date), 
+                $this->start_at, 
+                $this->owner_id, 
+                self::setGroup($this->group)
+            );
 
             Response::send(['response' => $totals], true, 200);
         } catch (\Exception $ex) {
@@ -35,6 +50,34 @@ class Reports
         } catch (\Throwable $th) {
             Response::send(['message' => 'bad request'], true, 400);
         }
+    }
+
+    private function setGroup(string $group = null)
+    {
+        if ($group == null) 
+            return array();
+
+        $group = explode(';', $group);
+        $group = array_map('strtoupper', $group);
+
+        $group_by = array();
+        if (in_array(Parameters::$REPORTS_KEY_GROUP_DAY, $group)) {
+            $group_by[] = Parameters::$GROUP_BY_KEY_DAY;
+        }
+        if (in_array(Parameters::$REPORTS_KEY_GROUP_MONTH, $group)) {
+            $group_by[] = Parameters::$GROUP_BY_KEY_MONTH;
+        }
+
+        if (in_array(Parameters::$REPORTS_KEY_GROUP_OWNER, $group)) {
+            $group_by[] = Parameters::$GROUP_BY_KEY_OWNER;
+        }
+        if (in_array(Parameters::$REPORTS_KEY_GROUP_PAYMENTMETHOD, $group)) {
+            $group_by[] = Parameters::$GROUP_BY_KEY_PAYMENTMETHOD;
+        }
+        if (in_array(Parameters::$REPORTS_KEY_GROUP_TRANSACTIONTYPE, $group)) {
+            $group_by[] = Parameters::$GROUP_BY_KEY_TRANSACTIONTYPE;
+        }
+        return $group_by;
     }
 
 }
--- src/model/dataAccess/Reports.php
+++ src/model/dataAccess/Reports.php
@@ -14,16 +14,92 @@ class Reports extends DataAccessObject
         parent::__construct();
     }
 
-    public function calculatesWalletsTotals_byPeriod(DateTime $start_date, DateTime $end_date, int $owner)
+    public function calculatesTotals_sql(DateTime $start_date, DateTime $end_date, int $start_day_of_month, int $owner_id, array $group_by = null)
     {
         try {
-            $sql  = "CALL finance_api.sum_wallets_by_period(:start_date, :end_date, :owner)";
+            $group_by_day = in_array(Parameters::$GROUP_BY_KEY_DAY, $group_by);
+            $group_by_month = in_array(Parameters::$GROUP_BY_KEY_MONTH, $group_by);
+            $group_by_payment_method = in_array(Parameters::$GROUP_BY_KEY_PAYMENTMETHOD, $group_by);
+            $group_by_transaction_type = in_array(Parameters::$GROUP_BY_KEY_TRANSACTIONTYPE, $group_by);
+
+            $select  = "select ";
+            $select .= "i.duo_date, ";
+            $select .= "CONCAT(DATE_FORMAT(CASE WHEN DATE_FORMAT(i.duo_date, '%d') >= :start_day_of_month_int THEN i.duo_date ELSE DATE_SUB(i.duo_date, INTERVAL 1 MONTH) END, '%Y-%m-'), :start_day_of_month_str) as start_at, ";
+            $select .= "w.id as wallet_id, ";
+            $select .= "w.name as wallet_name, ";
+            $select .= "pm.id as payment_method_id, ";
+            $select .= "pm.name as payment_method, ";
+            $select .= "tt.id as transaction_type_id, ";
+            $select .= "tt.name as transaction_type, ";
+            $select .= "sum(case when w.id = i.destination_wallet then (((i.gross_value + i.interest_value) - i.discount_value) + i.rounding_value) else 0 end) values_in, ";
+            $select .= "sum(case when w.id = i.source_wallet then (((i.gross_value + i.interest_value) - i.discount_value) + i.rounding_value) else 0 end) values_out, ";
+            $select .= "( ";
+            $select .= "sum(case when w.id = i.destination_wallet then (((i.gross_value + i.interest_value) - i.discount_value) + i.rounding_value) else 0 end) -  ";
+            $select .= "sum(case when w.id = i.source_wallet then (((i.gross_value + i.interest_value) - i.discount_value) + i.rounding_value) else 0 end) ";
+            $select .= ") as values_total ";
+
+            $from  = "from ";
+            $from .= "transaction t ";
+            $from .= "left join installment i on i.transaction = t.id ";
+            $from .= "left join wallet w on w.id = i.source_wallet or w.id = i.destination_wallet ";
+            $from .= "left join payment_method pm on pm.id = i.payment_method ";
+            $from .= "left join transaction_type tt on tt.id = t.transaction_type ";
+
+            $where  = "where ";
+            $where .= "w.owner_id = :owner_id ";
+            $where .= "and i.duo_date between :start_date and :end_date ";
+
+            $group  = "group by ";
+            if ($group_by_day) $group .= "duo_date, ";
+            if ($group_by_month) $group .= "start_at, ";
+            $group .= "w.id ";
+
+            $order  = "order by ";
+            $order .="duo_date, ";
+            $order .= "w.id;";
+            
+            $sql = "$select $from $where $group $order";
+            $stmt = self::getPDO()->prepare($sql);
+            $start_date = $start_date->format('Y-m-d');
+            $end_date = $end_date->format('Y-m-d');
+            $start_day_of_month_int = $start_day_of_month;
+            $start_day_of_month_str = sprintf("%'.02d", $start_day_of_month);
+            $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
+            $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
+            $stmt->bindParam(':owner_id', $owner_id, PDO::PARAM_INT);
+            $stmt->bindParam(':start_day_of_month_int', $start_day_of_month_int, PDO::PARAM_INT);
+            $stmt->bindParam(':start_day_of_month_str', $start_day_of_month_str, PDO::PARAM_STR);
+
+            $result = array();
+            if ($stmt->execute()) {
+                while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+                    $total = ['in' => $row->values_in, 'out' => $row->values_out, 'total' => $row->values_total];
+                    if ($group_by_day) {
+                        $result[$row->duo_date][$row->wallet_id] = $total;
+                    } elseif ($group_by_month) {
+                        $result[$row->start_at][$row->wallet_id] = $total;
+                    } else {
+                        $result[$row->wallet_id] = $total;
+                    }
+                }
+            }
+
+            return $result;
+        } catch (\Throwable $th) {
+            throw new UncatalogedException('An error occurred while looking for an \'owner\'. Please inform support', 1202002011);
+        }
+    }
+
+    public function calculatesWalletsTotals_byPeriod(DateTime $start_date, DateTime $end_date, int $owner_id)
+    {
+        try {
+            $sql  = "CALL finance_api.sum_wallets_by_period(:start_date, :end_date, :owner_id)";
             $stmt = self::getPDO()->prepare($sql);
             $start_date = $start_date->format('Y-m-d');
             $end_date = $end_date->format('Y-m-d');
             $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
             $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
-            $stmt->bindParam(':owner', $owner, PDO::PARAM_INT);
+            $stmt->bindParam(':owner_id', $owner_id, PDO::PARAM_INT);
 
             $result = array();
             if ($stmt->execute()) {
@@ -38,10 +114,10 @@ class Reports extends DataAccessObject
         }
     }
 
-    public function calculatesWalletsTotals_byMonths(DateTime $start_date, DateTime $end_date, int $owner)
+    public function calculatesWalletsTotals_byMonths(DateTime $start_date, DateTime $end_date, int $owner_id)
     {
         try {
-            $sql  = "CALL finance_api.sum_wallets_by_months(:start_date, :end_date, :start_day, :owner)";
+            $sql  = "CALL finance_api.sum_wallets_by_months(:start_date, :end_date, :start_day, :owner_id)";
             $stmt = self::getPDO()->prepare($sql);
             $start_date = $start_date->format('Y-m-d');
             $end_date = $end_date->format('Y-m-d');
@@ -49,7 +125,7 @@ class Reports extends DataAccessObject
             $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
             $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
             $stmt->bindParam(':start_day', $start_day, PDO::PARAM_INT);
-            $stmt->bindParam(':owner', $owner, PDO::PARAM_INT);
+            $stmt->bindParam(':owner_id', $owner_id, PDO::PARAM_INT);
 
             $result = array();
             if ($stmt->execute()) {
@@ -64,16 +140,16 @@ class Reports extends DataAccessObject
         }
     }
 
-    public function calculatesWalletsTotals_byDays(DateTime $start_date, DateTime $end_date, int $owner)
+    public function calculatesWalletsTotals_byDays(DateTime $start_date, DateTime $end_date, int $owner_id)
     {
         try {
-            $sql  = "CALL finance_api.sum_wallets_by_days(:start_date, :end_date, :owner)";
+            $sql  = "CALL finance_api.sum_wallets_by_days(:start_date, :end_date, :owner_id)";
             $stmt = self::getPDO()->prepare($sql);
             $start_date = $start_date->format('Y-m-d');
             $end_date = $end_date->format('Y-m-d');
             $stmt->bindParam(':start_date', $start_date, PDO::PARAM_STR);
             $stmt->bindParam(':end_date', $end_date, PDO::PARAM_STR);
-            $stmt->bindParam(':owner', $owner, PDO::PARAM_INT);
+            $stmt->bindParam(':owner_id', $owner_id, PDO::PARAM_INT);
 
             $result = array();
             if ($stmt->execute()) {
